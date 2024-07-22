b18f2e7a5a1b21cacc0396d87a87feec337ac282
Muri <mcsanchezlaguna@gmail.com>
2024-07-22 15:36:37 +0200

    added funcionality for the drop down

--- src/index.js
+++ src/index.js
@@ -1,55 +1,87 @@
 function updateTime() {
   //For "Ciudad de Panama" real time data in (timezone - America/Panama):
-  let timeZoneOne = "America/Panama";
-  let panamaFormatDate = moment().tz(timeZoneOne).format("MMMM Do YYYY");
-  let panamaCurrentDate = `Panamá 🇵🇦 - ${panamaFormatDate}`;
-  let panamaCurrentTime = moment()
-    .tz(timeZoneOne)
-    .format("h:mm:ss [<small>]A[</small>]");
-
   let panamaElement = document.querySelector("#panama");
-  let panamaDateElement = panamaElement.querySelector(".date");
-  let panamaTimeElement = panamaElement.querySelector(".time");
+  if (panamaElement) {
+    let timeZoneOne = "America/Panama";
+    let panamaFormatDate = moment().tz(timeZoneOne).format("MMMM Do YYYY");
+    let panamaCurrentDate = `Panamá 🇵🇦 - ${panamaFormatDate}`;
+    let panamaCurrentTime = moment()
+      .tz(timeZoneOne)
+      .format("h:mm:ss [<small>]A[</small>]");
 
-  panamaDateElement.innerHTML = panamaCurrentDate;
-  panamaTimeElement.innerHTML = panamaCurrentTime;
+    let panamaDateElement = panamaElement.querySelector(".date");
+    let panamaTimeElement = panamaElement.querySelector(".time");
+
+    panamaDateElement.innerHTML = panamaCurrentDate;
+    panamaTimeElement.innerHTML = panamaCurrentTime;
+  }
 
   //For "Castellón de la Plana" real time data in (timezone - Europe/Madrid):
-  let timeZoneTwo = "Europe/Madrid";
-  let castellonFormatDate = moment().tz(timeZoneTwo).format("MMMM Do YYYY");
-  let castellonCurrentDate = `España 🇪🇸 - ${castellonFormatDate}`;
-  let castellonCurrentTime = moment()
-    .tz(timeZoneTwo)
-    .format("h:mm:ss [<small>]A[</small>]");
-
   let castellonElement = document.querySelector("#castellon");
-  let castellonDateElement = castellonElement.querySelector(".date");
-  let castellonTimeElement = castellonElement.querySelector(".time");
+  if (castellonElement) {
+    let timeZoneTwo = "Europe/Madrid";
+    let castellonFormatDate = moment().tz(timeZoneTwo).format("MMMM Do YYYY");
+    let castellonCurrentDate = `España 🇪🇸 - ${castellonFormatDate}`;
+    let castellonCurrentTime = moment()
+      .tz(timeZoneTwo)
+      .format("h:mm:ss [<small>]A[</small>]");
 
-  castellonDateElement.innerHTML = castellonCurrentDate;
-  castellonTimeElement.innerHTML = castellonCurrentTime;
+    let castellonDateElement = castellonElement.querySelector(".date");
+    let castellonTimeElement = castellonElement.querySelector(".time");
+
+    castellonDateElement.innerHTML = castellonCurrentDate;
+    castellonTimeElement.innerHTML = castellonCurrentTime;
+  }
 
   //For "Oporto" real time data in (timezone - Portugal):
-  let timeZoneThree = "Portugal";
-  let oportoFormatDate = moment().tz(timeZoneThree).format("MMMM Do YYYY");
-  let oportoCurrentDate = `Portugal 🇵🇹 - ${oportoFormatDate}`;
-  let oportoCurrentTime = moment()
-    .tz(timeZoneThree)
-    .format("h:mm:ss [<small>]A[</small>]");
-
   let oportoElement = document.querySelector("#oporto");
-  let oportoDateElement = oportoElement.querySelector(".date");
-  let oportoTimeElement = oportoElement.querySelector(".time");
+  if (oportoElement) {
+    let timeZoneThree = "Portugal";
+    let oportoFormatDate = moment().tz(timeZoneThree).format("MMMM Do YYYY");
+    let oportoCurrentDate = `Portugal 🇵🇹 - ${oportoFormatDate}`;
+    let oportoCurrentTime = moment()
+      .tz(timeZoneThree)
+      .format("h:mm:ss [<small>]A[</small>]");
 
-  oportoDateElement.innerHTML = oportoCurrentDate;
-  oportoTimeElement.innerHTML = oportoCurrentTime;
+    let oportoDateElement = oportoElement.querySelector(".date");
+    let oportoTimeElement = oportoElement.querySelector(".time");
+
+    oportoDateElement.innerHTML = oportoCurrentDate;
+    oportoTimeElement.innerHTML = oportoCurrentTime;
+  }
+
+  //For "Pueblo Viejo" real time data in (timezone - America/Dominica):
+  //For "Arizona" real time data in (timezone - US/Arizona):
+  //For "Montreal" (Toronto, Montreal, Quebec City and Ottawa) real time data in (timezone - America/Montreal):
+  //For "México (Sonora, Hermosillo)" real time data in (timezone - America/Hermosillo):
+  //For "México (Monterrey, Guerrero, Torreón, Chihuahua)" real time data in (timezone - America/Monterrey):
+  //For "México (Monterrey, Guerrero, Torreón, Chihuahua)" real time data in (timezone - America/Monterrey):
+
+  function updateCity(event) {
+    let cityTimeZoneValue = event.target.value;
+    let cityName = cityTimeZoneValue.replace("_", " ").split("/")[1];
+
+    let cityFormatDate = moment().tz(cityTimeZoneValue).format("MMMM Do YYYY");
+    let cityCurrentDate = `${cityFormatDate}`;
+
+    let cityCurrentTime = moment()
+      .tz(cityTimeZoneValue)
+      .format("h:mm:ss [<small>]A[</small>]");
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+        <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityCurrentDate}</div>
+          </div>
+          <div class="time">${cityCurrentTime}</div>
+        </div>
+  `;
+  }
+
+  let selectCityElement = document.querySelector("#city_drop_down");
+  selectCityElement.addEventListener("change", updateCity);
 }
 
 updateTime();
 setInterval(updateTime, 1000);
-
-//For "Pueblo Viejo" real time data in (timezone - America/Dominica):
-//For "Arizona" real time data in (timezone - US/Arizona):
-//For "Montreal" (Toronto, Montreal, Quebec City and Ottawa) real time data in (timezone - America/Montreal):
-//For "México (Sonora, Hermosillo)" real time data in (timezone - America/Hermosillo):
-//For "México (Monterrey, Guerrero, Torreón, Chihuahua)" real time data in (timezone - America/Monterrey):
