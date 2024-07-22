bec97c897b5f5e64a0d07c2f9583271ce69a31c4
Muri <mcsanchezlaguna@gmail.com>
2024-07-22 12:37:59 +0200

    add a set interval

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
-//Panama
-let panamaFormatDate = moment().tz("America/Panama").format("MMMM Do YYYY");
-let panamaCurrentDate = `Panamá 🇵🇦 - ${panamaFormatDate}`;
-let panamaCurrentTime = moment().tz("America/Panama").format("h:mm:ss");
-let panamaCurrentTimeAMPM = moment().tz("America/Panama").format(" A");
+function updateTime() {
+  //For "Ciudad de Panama" real time data in (timezone - America/Panama):
+  let timeZone = "America/Panama";
+  let panamaFormatDate = moment().tz(timeZone).format("MMMM Do YYYY");
+  let panamaCurrentDate = `Panamá 🇵🇦 - ${panamaFormatDate}`;
+  let panamaCurrentTime = moment()
+    .tz("timeZone")
+    .format("h:mm:ss [<small>]A[</small>]");
 
-let panamaElement = document.querySelector("#panama");
-let panamaDateElement = panamaElement.querySelector(".date");
-let panamaTimeElement = panamaElement.querySelector(".time");
-panamaDateElement.innerHTML = panamaCurrentDate;
-panamaTimeElement.innerHTML = `${panamaCurrentTime} <small>${panamaCurrentTimeAMPM}</small>`;
+  let panamaElement = document.querySelector("#panama");
+  let panamaDateElement = panamaElement.querySelector(".date");
+  let panamaTimeElement = panamaElement.querySelector(".time");
+
+  panamaDateElement.innerHTML = panamaCurrentDate;
+  panamaTimeElement.innerHTML = panamaCurrentTime;
+
+  //For "Castellón de la Plana" real time data in (timezone - Europe/Madrid):
+
+  //For "Oporto" real time data in (timezone - Portugal):
+}
+
+updateTime();
+setInterval(updateTime, 1000);
