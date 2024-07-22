87945f2263d21418cf5c64409e213c6f0ebb6851
Muri <mcsanchezlaguna@gmail.com>
2024-07-22 14:14:36 +0200

    Added date and time to the other 2 cities

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 function updateTime() {
   //For "Ciudad de Panama" real time data in (timezone - America/Panama):
-  let timeZone = "America/Panama";
-  let panamaFormatDate = moment().tz(timeZone).format("MMMM Do YYYY");
+  let timeZoneOne = "America/Panama";
+  let panamaFormatDate = moment().tz(timeZoneOne).format("MMMM Do YYYY");
   let panamaCurrentDate = `Panamá 🇵🇦 - ${panamaFormatDate}`;
   let panamaCurrentTime = moment()
-    .tz(timeZone)
+    .tz(timeZoneOne)
     .format("h:mm:ss [<small>]A[</small>]");
 
   let panamaElement = document.querySelector("#panama");
@@ -15,8 +15,34 @@ function updateTime() {
   panamaTimeElement.innerHTML = panamaCurrentTime;
 
   //For "Castellón de la Plana" real time data in (timezone - Europe/Madrid):
+  let timeZoneTwo = "Europe/Madrid";
+  let castellonFormatDate = moment().tz(timeZoneTwo).format("MMMM Do YYYY");
+  let castellonCurrentDate = `España 🇪🇸 - ${castellonFormatDate}`;
+  let castellonCurrentTime = moment()
+    .tz(timeZoneTwo)
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  let castellonElement = document.querySelector("#castellon");
+  let castellonDateElement = castellonElement.querySelector(".date");
+  let castellonTimeElement = castellonElement.querySelector(".time");
+
+  castellonDateElement.innerHTML = castellonCurrentDate;
+  castellonTimeElement.innerHTML = castellonCurrentTime;
 
   //For "Oporto" real time data in (timezone - Portugal):
+  let timeZoneThree = "Portugal";
+  let oportoFormatDate = moment().tz(timeZoneThree).format("MMMM Do YYYY");
+  let oportoCurrentDate = `Portugal 🇵🇹 - ${oportoFormatDate}`;
+  let oportoCurrentTime = moment()
+    .tz(timeZoneThree)
+    .format("h:mm:ss [<small>]A[</small>]");
+
+  let oportoElement = document.querySelector("#oporto");
+  let oportoDateElement = oportoElement.querySelector(".date");
+  let oportoTimeElement = oportoElement.querySelector(".time");
+
+  oportoDateElement.innerHTML = oportoCurrentDate;
+  oportoTimeElement.innerHTML = oportoCurrentTime;
 }
 
 updateTime();
